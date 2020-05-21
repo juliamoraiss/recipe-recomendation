61682157aac93889fdba269b5a48fdd9a7e4210e
Júlia <juliamorais03@gmail.com>
2020-05-21 18:44:48 -0300

    sei la

--- app/controllers/default.py
+++ app/controllers/default.py
@@ -6,12 +6,7 @@ import pandas as pd
 from fuzzywuzzy import fuzz
 import sqlalchemy as db
 import os
-from flask import Flask 
-  
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
-app.config['SECRET_KEY'] = os.environ.get('FLASK_SECRET_KEY')
+
 
 @app.route('/')
 def index():
--- wsgi.py
+++
@@ -1,4 +0,0 @@
-from app.controllers.default import app 
-  
-if __name__ == "__main__": 
-        app.run() 
